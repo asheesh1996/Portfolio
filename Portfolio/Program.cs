3aef8be9ff52b58d4e7849ef902c67a938ec7b8b
Asheesh Maheshwari <asheeshagra@gmail.com>
2022-05-15 15:09:02 +0530

    WIP : added UR3 page

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Portfolio;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
