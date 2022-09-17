49d9f64a8e460abe9b4e600863ec5fa217f96941
Asheesh Maheshwari <asheeshagra@gmail.com>
2022-09-17 12:57:03 +0530

    WIP

---
+++ Portfolio/Services/Test.cs
@@ -0,0 +1,46 @@
+﻿namespace Portfolio.Services
+{
+    public class Test
+    {
+    }
+
+    public enum LogType
+    {
+        LetterLog,
+        DigitLog,
+    }
+    public class Log : IComparer<Log>
+    {
+
+        public Log(string log)
+        {
+            _log = log;
+            tokensArray = log.Split(' ');
+            if (int.TryParse(tokensArray[1], out int val))
+            {
+                logType = LogType.DigitLog;
+            }
+            else
+            {
+                logType = LogType.LetterLog;
+            }
+
+          
+        }
+        public LogType logType { get; }
+        public string _log;
+        public string[] tokensArray { get; }
+        public int Compare(Log x, Log y)
+        {
+            int index = 1;
+            int result = 0;
+            
+            while ((result = string.Compare(x.tokensArray[index], y.tokensArray[index])) == 0)
+            {
+                index++;
+            }
+            return result;
+        }
+    }
+
+}
